393920f2f4810329412bf87fb69196890e769096
Frasenk <kubos7@gmail.com>
2022-07-27 21:01:48 +0200

    added AR

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
+# MIT License
+#
+# Copyright (c) 2017 François Chollet
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+
 import tensorflow as tf
+import numpy as np
 
 
 def import_data():
@@ -30,6 +54,12 @@ def train_ml(train_images, train_labels, test_labels, test_images):
     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
     print('\nTest accuracy:', test_acc)
+
+    probability_model = tf.keras.Sequential([model,
+                                             tf.keras.layers.Softmax()])
+    predictions = probability_model.predict(test_images)
+    print(predictions[0])
+    print(np.argmax(predictions[0]))
 
 
 if __name__ == '__main__':
